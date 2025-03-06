89e1b50bdbcc7581cd48343633056fc4a91c0795
Poseidon <122105735+poseidonxy@users.noreply.github.com>
2025-03-06 14:06:00 +0100

    types config

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,8 +1,27 @@
 //d f
-import { app, BrowserWindow } from "electron";
-import path from "path";
+import { app, BrowserWindow, ipcMain } from "electron";
+import { ipcMainHandle, isDev } from "./util.js";
+import { getPreloadPath, getUIPath } from "./pathResolver.js";
+import { getStaticData, pollResources } from "./resourseManager.js";
 
+app.commandLine.appendSwitch("disable-gpu");
+app.commandLine.appendSwitch("disable-software-rasterizer");
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
-  mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath(),
+    },
+    // disables default system frame (dont do this if you want a proper working menu bar)
+    frame: false,
+  });
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5123");
+  } else {
+    mainWindow.loadFile(getUIPath());
+  }
+
+  // pollResources(mainWindow);
+  ipcMainHandle("getStaticData", () => {
+    return getStaticData();
+  });
 });
